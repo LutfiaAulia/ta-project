6ca79f4174907d0a97e61e072ee899580e6ac4bd
LutfiaAulia <lutfiaaulia2@gmail.com>
2025-04-15 14:14:42 +0700

    login

--- resources/js/Pages/Auth/Login.tsx
+++ resources/js/Pages/Auth/Login.tsx
@@ -1,11 +1,7 @@
-import Checkbox from '@/Components/Checkbox';
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
-import GuestLayout from '@/Layouts/GuestLayout';
-import { Head, Link, useForm } from '@inertiajs/react';
-import { FormEventHandler } from 'react';
+import InputError from "@/Components/InputError";
+import TextInput from "@/Components/TextInput";
+import { Head, Link, useForm } from "@inertiajs/react";
+import { FormEventHandler } from "react";
 
 export default function Login({
     status,
@@ -15,96 +11,127 @@ export default function Login({
     canResetPassword: boolean;
 }) {
     const { data, setData, post, processing, errors, reset } = useForm({
-        email: '',
-        password: '',
+        email: "",
+        password: "",
         remember: false as boolean,
     });
 
     const submit: FormEventHandler = (e) => {
         e.preventDefault();
 
-        post(route('login'), {
-            onFinish: () => reset('password'),
+        post(route("login"), {
+            onFinish: () => reset("password"),
         });
     };
 
     return (
-        <GuestLayout>
+        <>
             <Head title="Log in" />
 
-            {status && (
-                <div className="mb-4 text-sm font-medium text-green-600">
-                    {status}
-                </div>
-            )}
+            <div className="min-h-screen flex items-center justify-center bg-gray-100">
+                <div className="w-full max-w-4xl bg-white rounded-xl shadow-lg overflow-hidden grid grid-cols-1 md:grid-cols-2">
+                    {/* Bagian Kiri */}
+                    <div className="p-8">
+                        <div className="flex flex-col items-center mb-6">
+                            <img
+                                src="/logo.png"
+                                alt="Logo"
+                                className="h-16 mb-2"
+                            />
+                            <p className="text-center font-medium text-sm">
+                                Dinas Koperasi UKM Provinsi <br /> Sumatera
+                                Barat
+                            </p>
+                        </div>
 
-            <form onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="email" value="Email" />
+                        <h2 className="text-2xl font-extrabold text-center mb-6">
+                            Masuk
+                        </h2>
 
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        isFocused={true}
-                        onChange={(e) => setData('email', e.target.value)}
-                    />
+                        {status && (
+                            <div className="mb-4 text-sm font-medium text-green-600">
+                                {status}
+                            </div>
+                        )}
 
-                    <InputError message={errors.email} className="mt-2" />
-                </div>
+                        <form onSubmit={submit}>
+                            <div>
+                                <TextInput
+                                    id="email"
+                                    type="email"
+                                    name="email"
+                                    value={data.email}
+                                    className="w-full px-4 py-2 border rounded-lg bg-gray-100"
+                                    placeholder="email"
+                                    autoComplete="username"
+                                    onChange={(e) =>
+                                        setData("email", e.target.value)
+                                    }
+                                />
+                                <InputError
+                                    message={errors.email}
+                                    className="mt-2"
+                                />
+                            </div>
 
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
+                            <div className="mt-4">
+                                <TextInput
+                                    id="password"
+                                    type="password"
+                                    name="password"
+                                    value={data.password}
+                                    className="w-full px-4 py-2 border rounded-lg bg-gray-100"
+                                    placeholder="password"
+                                    autoComplete="current-password"
+                                    onChange={(e) =>
+                                        setData("password", e.target.value)
+                                    }
+                                />
+                                <InputError
+                                    message={errors.password}
+                                    className="mt-2"
+                                />
+                                {canResetPassword && (
+                                    <div className="text-right text-sm mt-2">
+                                        <Link
+                                            href={route("password.request")}
+                                            className="text-gray-500 hover:text-gray-800"
+                                        >
+                                            lupa password?
+                                        </Link>
+                                    </div>
+                                )}
+                            </div>
 
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="current-password"
-                        onChange={(e) => setData('password', e.target.value)}
-                    />
+                            <div className="mt-6 flex justify-center">
+                                <button
+                                    className="px-10 py-2.5 bg-green-500 hover:bg-green-600 text-white font-bold py-2 rounded-full"
+                                    disabled={processing}
+                                >
+                                    Masuk
+                                </button>
+                            </div>
+                        </form>
+                    </div>
 
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
-
-                <div className="mt-4 block">
-                    <label className="flex items-center">
-                        <Checkbox
-                            name="remember"
-                            checked={data.remember}
-                            onChange={(e) =>
-                                setData(
-                                    'remember',
-                                    (e.target.checked || false) as false,
-                                )
-                            }
-                        />
-                        <span className="ms-2 text-sm text-gray-600">
-                            Remember me
-                        </span>
-                    </label>
-                </div>
-
-                <div className="mt-4 flex items-center justify-end">
-                    {canResetPassword && (
+                    {/* Bagian Kanan */}
+                    <div className="bg-green-500 text-white p-10 flex flex-col justify-center items-center">
+                        <h2 className="text-2xl font-extrabold mb-4">
+                            Selamat Datang
+                        </h2>
+                        <p className="text-center mb-6 text-sm">
+                            "Daftar sekarang untuk mendapatkan akses pemesanan
+                            Mobil Klinik dengan mudah dan cepat!"
+                        </p>
                         <Link
-                            href={route('password.request')}
-                            className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                            href={route("register")}
+                            className="border border-white px-6 py-2 rounded-full font-medium hover:bg-white hover:text-green-600 transition"
                         >
-                            Forgot your password?
+                            Registrasi
                         </Link>
-                    )}
-
-                    <PrimaryButton className="ms-4" disabled={processing}>
-                        Log in
-                    </PrimaryButton>
+                    </div>
                 </div>
-            </form>
-        </GuestLayout>
+            </div>
+        </>
     );
 }
